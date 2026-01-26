e288696206f13adb1d1b091780abcdd24476d259
alinatroglio <alinatroglio@web.de>
2026-01-26 08:58:46 +0100

    Update copyright and license
    Add copyright information and licensing details.

--- code/gui/searchpages.py
+++ code/gui/searchpages.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+# Based on python-odmltables
+# Copyright (c) 2015–2023 Forschungszentrum Jülich GmbH
+# Modifications © 2023 Alina Troglio / Namer's Lab
+# Licensed under the BSD 3-Clause License
 
 import copy
 import os
